f43b66063d61e7c6675a9de6ede6a1920835fd24
Sam Beckett <sambeckett@Sams-MacBook-Pro.local>
2024-02-26 11:32:54 -0700

    Fix slideshow height issue on mobile
    - Added --doc-height CSS variable
    - Added JS code to tiled-slideshow to set and update --doc-height

--- src/assets/js/tiled-slideshow.js
+++ src/assets/js/tiled-slideshow.js
@@ -1,3 +1,10 @@
+const documentHeight = () => {
+	const doc = document.documentElement
+	doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', documentHeight);
+documentHeight();
+
 Vue.component('tiled-slideshow',
 {
 	props:
